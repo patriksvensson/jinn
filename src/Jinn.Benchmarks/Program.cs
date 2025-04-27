760d17a1eb5fc52ca19becc7afc0fdf3bf1d3470
Patrik Svensson <patrik@patriksvensson.se>
2025-04-27 23:04:09 +0200

    Add benchmarks for parsing

---
+++ src/Jinn.Benchmarks/Program.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Jinn;
+
+namespace Jinn.Benchmarks;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        BenchmarkRunner.Run(typeof(Program).Assembly);
+        return 0;
+    }
+}
+
+[MemoryDiagnoser]
+public class ParserBenchmarks
+{
+    private readonly RootCommand _root;
+
+    public ParserBenchmarks()
+    {
+        var bar = new Command("bar");
+        bar.AddOption(new Option<bool>("--baz"));
+        bar.AddOption(new Option<int>("--qux"));
+
+        var foo = new Command("foo");
+        foo.AddArgument(new Argument<int>("VALUE"));
+        foo.AddOption(new Option<bool>("--flag"));
+        foo.AddCommand(bar);
+
+        _root = new RootCommand(foo);
+    }
+
+    [Benchmark]
+    public void Jinn_Parse() => _root.Parse(
+        ["foo", "42", "--flag", "bar", "--baz", "--qux", "32"]);
+}
